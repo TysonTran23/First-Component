19bf02f064f28307a4b27f6f30f66acb338b3b49
Tyson <trantyson@icloud.com>
2023-09-25 17:07:24 -0700

    commit

---
+++ Shiba.js
@@ -0,0 +1,7 @@
+
+
+const Shiba = () => {
+    return <img src="https://media.istockphoto.com/id/824846640/photo/red-shiba-inu-dog-on-isolated-white-background.webp?b=1&s=170667a&w=0&k=20&c=oTuhRC7K8CRFNsP5Z3ZWavRlEbOUHwgIAxlDLMa29kE=">
+}
+
+ReactDom.render(<Shiba/>, document.getElementById("root"))
